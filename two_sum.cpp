51c3bf1c0ed888cac8f73aaead45976f4b92f099
santysharmaa <santy.sharma2572@gmail.com>
2026-01-29 22:00:59 +0530

    added two_sum.cpp

---
+++ two_sum.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool trg (vector<int> arr, int t ){
+    
+    unordered_set<int> hashTable;
+    for(int i{0}; i<arr.size(); i++){
+        int temp = t - arr[i];
+        if(hashTable.find(temp) != hashTable.end()){
+            return true;
+        }
+        else{
+            hashTable.insert(arr[i]);
+        }
+    }
+    return false;
+}
+
+int main(){
+    vector<int> arr = {1, 3, 4, 2, 8};
+    int t = 7;
+    cout<<(trg(arr, t) ? "True" : "False");
+    return 0;
+}
