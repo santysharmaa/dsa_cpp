77208786bbd4053ea7feb341b7ecbcb3cf238061
santysharmaa <santy.sharma2572@gmail.com>
2026-01-31 23:28:10 +0530

    added two more questipons

---
+++ triplets_sum_in_array.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool triplet(vector<int>& x, int trg){
+    
+
+}
+
+int main(){
+    vector<int> x = {1, 4, 45, 6, 10, 8};
+    int trg = 13;
+    cout<<(triplet(x, trg) ? "True" : "False");
+    return 0;
+}
