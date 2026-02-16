33de7d4f9f2bb04e8d7419368cd2991fb3a6c9ed
santysharmaa <santy.sharma2572@gmail.com>
2026-02-16 16:48:08 +0530

    addded a gfg question

---
+++ Stacks and Queues/valid_parenthesis.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool valid_parenthesis(string s){
+
+
+    
+
+}
+
+int main() {
+    string s ={"{ ] }"};
+
+    cout<<(valid_parenthesis(s) ? "True" : "False");
+
+    return 0;
+}
