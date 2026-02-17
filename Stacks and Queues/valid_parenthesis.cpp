5cb962a5a3746480dc69bafeb4931fbd9a23ab34
santysharmaa <santy.sharma2572@gmail.com>
2026-02-17 13:45:53 +0530

    added a valid parenthesis

--- Stacks and Queues/valid_parenthesis.cpp
+++ Stacks and Queues/valid_parenthesis.cpp
@@ -1,17 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool valid_parenthesis(string s){
+bool isValid(string s) {
+    stack<char> st;
 
-
-    
-
+    for(int  i {0}; i<s.size(); i++){
+        if( (s[i] == '(')  || (s[i] == '{')  ||  (s[i] == '[') ) st.push(s[i]);
+        else{
+            if( st.empty() ) return false;
+            if( (st.top() == '(' && s[i] == ')' ) ||  (st.top() == '{' && s[i] == '}' ) || (st.top() == '[' && s[i] == ']' ) ) st.pop();
+            else return false;
+        }
+    }
+    return st.empty();
 }
 
 int main() {
-    string s ={"{ ] }"};
+    string s ={" ( )"};
 
-    cout<<(valid_parenthesis(s) ? "True" : "False");
+    cout<<(isValid(s) ? "True" : "False");
 
     return 0;
 }
